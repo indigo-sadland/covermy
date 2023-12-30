9063de3ff1b715ef34ed726fedb76c5af21392cb
indigo-sadland <37074372+indigo-sadland@users.noreply.github.com>
2023-12-30 08:50:12 +0000

    new normalizer rules added

--- utils/normalizer/normalizer.go
+++ utils/normalizer/normalizer.go
@@ -12,12 +12,21 @@ const ffuftail = "[0m"
 const ffuflongspace = "                    "
 const ffufesc = "\u001B"
 
+const gobusteresc = "\u001B"
+const gobustermidle = `^\r\[2K`
+
 func Start(tool string, rawlines []string) []string {
 	var normilized []string
 
 	switch tool {
 	case "ffuf":
 		normilized = ffuf(rawlines)
+	case "gobuster":
+		normilized = gobuster(rawlines)	
+	case "rustscan":
+		normilized = rustscan(rawlines)
+	case "nuclei":
+		normilized = nuclei(rawlines)	
 	default:
 		fmt.Printf("No normalization rules for the %s found.\n "+
 			"Feel free to leave a request!", tool)
@@ -41,3 +50,51 @@ func ffuf(rawlines []string) []string {
 
 	return lines
 }
+
+func gobuster(rawlines []string) []string {
+	var lines []string
+
+	for _, l := range rawlines {
+		l = strings.ReplaceAll(l, gobusteresc, "")
+		l = regexp.MustCompile(gobustermidle).ReplaceAllString(l, "")
+		lines = append(lines, l)
+	}
+
+	return lines
+}
+
+
+func rustscan(rawlines []string) []string {
+	var lines []string
+
+	for _, l := range rawlines {
+		l = strings.ReplaceAll(l, "\u001B", "")
+		l = regexp.MustCompile(`\[38;2;\d{1,3};\d{1,3};\d{1,3}m`).ReplaceAllString(l, "")
+		l = strings.ReplaceAll(l, "[1;34m", "")
+		l = strings.ReplaceAll(l, "[1;38;2;0;255;9m", "")
+
+		x := regexp.MustCompile(`(^\[.])(\[0m)`)
+		l = x.ReplaceAllString(l, "$1")
+
+		l = regexp.MustCompile(`^\[1;?.*?m`).ReplaceAllString(l, "")
+		l = regexp.MustCompile(`(\[0m)*$`).ReplaceAllString(l, "")
+		lines = append(lines, l)
+
+	}
+
+	return lines
+}
+
+func nuclei(rawlines []string) []string {
+	var lines []string
+
+	for _, l := range rawlines {
+		l = strings.ReplaceAll(l, "\u001B", "")
+		l = regexp.MustCompile(`\[1;?.*?m`).ReplaceAllString(l, "")
+		l = regexp.MustCompile(`\[\d{1,3}m`).ReplaceAllString(l, "")
+		lines = append(lines, l)
+	}
+
+	return lines
+}
+
