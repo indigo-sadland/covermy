d6e75727081ac50262a86360924fe37693f7506c
indigo-sadland <37074372+indigo-sadland@users.noreply.github.com>
2023-12-30 08:50:12 +0000

    Add files via upload

---
+++ cmd/cmd.go
@@ -0,0 +1,44 @@
+package cmd
+
+import (
+	"bufio"
+	"bytes"
+	"covermy/utils/logcamp"
+	"covermy/utils/normalizer"
+	"fmt"
+	"os/exec"
+	"strings"
+)
+
+func Run(command []string) []string {
+	var stderr bytes.Buffer
+	var rawlines []string
+
+	bin := command[0]
+	args := command[1:]
+	cmd := exec.Command(bin, args...)
+	cmd.Stderr = &stderr
+
+	cmdReader, err := cmd.StdoutPipe()
+	if err != nil {
+		logcamp.ErrorLogger.Printf(err.Error())
+	}
+	// Read cmd output in real-time
+	scanner := bufio.NewScanner(cmdReader)
+	// Add the tool command to the list. Needed for the finale output.
+	rawlines = append(rawlines, fmt.Sprintf("\n"+strings.Join(command, " ")+"\n"))
+	go func() {
+		for scanner.Scan() {
+			line := scanner.Text()
+			rawlines = append(rawlines, line)
+		}
+	}()
+
+	if err := cmd.Run(); err != nil {
+		fmt.Printf("\n")
+		fmt.Println(&stderr)
+	}
+
+	output := normalizer.Start(bin, rawlines)
+	return output
+}
---
+++ go.mod
@@ -0,0 +1,15 @@
+module covermy
+
+go 1.19
+
+require (
+	github.com/fatih/color v1.13.0
+	github.com/jpillora/go-tld v1.2.1
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.9 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+)
